e00fb4de19396ccc17e970604073fed9d9fe54b9
mayo-ya <mayo.yamasaki@gmail.com>
2017-05-06 21:09:19 +0900

    [Add] sum-of-multiples

---
+++ sum-of-multiples/src/lib.rs
@@ -0,0 +1,19 @@
+pub fn sum_of_multiples2(n: i64, nums: &Vec<i64>) -> i64 {
+    let mut result = 0;
+    for i in 1..n {
+        for j in nums {
+            if i % j == 0 {
+                result += i;
+                break;
+            }
+        }
+    }
+    result
+}
+
+
+pub fn sum_of_multiples(n: i64, nums: &Vec<i64>) -> i64 {
+    let is_multiple = |i: &i64| nums.iter().any(|&n| i % n == 0);
+    let result:i64 = (1..n).filter(is_multiple).sum();
+    result
+}
