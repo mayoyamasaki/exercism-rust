38cdfee9841866bf0184493ae09ac9d77c7a4399
mayo-ya <mayo.yamasaki@gmail.com>
2017-05-07 14:24:41 +0900

    [Add] scrabble-score

---
+++ scrabble-score/src/lib.rs
@@ -0,0 +1,18 @@
+use std::ascii::AsciiExt;
+
+fn point(c: char) -> u32 {
+    match c.to_ascii_lowercase() {
+        'a' | 'e' | 'i' | 'o' | 'u' | 'l' | 'n' | 'r' | 's' | 't' => 1,
+        'd' | 'g' => 2,
+        'b' | 'c' | 'm' | 'p' => 3,
+        'f' | 'h' | 'v' | 'w' | 'y' => 4,
+        'k' => 5,
+        'j' | 'x' => 8,
+        'q' | 'z' => 10,
+        _ => 0,
+    }
+}
+
+pub fn score(letter: &str) -> u32 {
+    letter.chars().map(point).sum()
+}
