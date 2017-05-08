ad857de0add71762af2839c2b61ae62ed6ce5023
mayo-ya <mayo.yamasaki@gmail.com>
2017-05-09 00:05:13 +0900

    [Mod] refactoring luhn

--- luhn/src/lib.rs
+++ luhn/src/lib.rs
@@ -1,12 +1,12 @@
 pub fn is_valid(digits: &str) -> bool {
-
-    let mut sum: u32 = 0;
-    let digits:String = digits.chars().filter(|c| !c.is_whitespace()).collect();
+    let digits:String = digits.chars()
+                              .filter(|c| !c.is_whitespace())
+                              .collect();
 
     if digits.chars().count() < 2 { return false; }
 
+    let mut sum: u32 = 0;
     for (i, c) in digits.chars().rev().enumerate() {
-        println!("{}", c);
         if let Some(n) = c.to_digit(10) {
             if i % 2 == 0 {
                 sum += n;
@@ -17,6 +17,6 @@ pub fn is_valid(digits: &str) -> bool {
             return false;
         }
     }
-    println!("{}", sum);
+
     sum % 10 == 0
 }
