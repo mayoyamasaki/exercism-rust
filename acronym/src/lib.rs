7ec266db6e7773cabd0e7fb46be9bd0995d96613
mayo-ya <mayo.yamasaki@gmail.com>
2017-05-13 15:29:44 +0900

    [Mod] rename

--- acronym/src/lib.rs
+++ acronym/src/lib.rs
@@ -2,9 +2,9 @@ pub fn abbreviate(msg: &str) -> String {
     let words = msg.split(|c: char| c.is_whitespace() || c == '-');
     let mut acronym  = String::new();
     for word in words {
-        let mut is_prev_uppercase = false;
+        let mut prev_is_uppercase = false;
         for (i, c) in word.chars().enumerate() {
-            if (i == 0 && c.is_alphabetic()) || (c.is_uppercase() && !is_prev_uppercase) {
+            if (i == 0 && c.is_alphabetic()) || (c.is_uppercase() && !prev_is_uppercase) {
                 acronym.push(c);
             }
             is_prev_uppercase = c.is_uppercase();
